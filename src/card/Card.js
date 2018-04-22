64b96b1247081a2dd5e4e75688591d13a4c3629c
sbroadhurst <stepbro31@gmail.com>
2018-04-22 13:36:44 -0500

    added search for trainer,items,and energy

--- src/card/Card.js
+++ src/card/Card.js
@@ -18,11 +18,56 @@ class Card extends React.Component {
       })
   }
 
+  cardResist() {
+    if (this.props.card.resistances) {
+      return this.props.card.resistances.map((info, index) => {
+        return (
+          <div key={index} style={{ display: 'inline-block' }}>
+            <p>
+              Resistance: {info.type} {info.value}
+            </p>
+          </div>
+        )
+      })
+    }
+  }
+
+  cardWeakness() {
+    if (this.props.card.weaknesses) {
+      return this.props.card.weaknesses.map((info, index) => {
+        return (
+          <div key={index} style={{ display: 'inline-block', marginLeft: 15 }}>
+            <p>
+              Weaknesses: {info.type} {info.value}
+            </p>
+          </div>
+        )
+      })
+    }
+  }
+
   render() {
     const { card } = this.props
     console.log(card)
     if (!card.attacks) {
-      return <div />
+      return (
+        <div className="wrapper">
+          <article className="main">
+            <div className="body">
+              {card.name} <br />
+              <br />
+              {card.text}
+            </div>
+          </article>
+          <aside className="aside aside-1">
+            <img
+              src={card.imageUrlHiRes}
+              className="card-image"
+              alt="The pokemon card selected"
+            />
+          </aside>
+        </div>
+      )
     }
     return (
       <div className="wrapper">
@@ -59,6 +104,11 @@ class Card extends React.Component {
               )
             })}
             <br />
+            {this.cardResist()}
+            {this.cardWeakness()}
+            <p style={{ display: 'inline-block', marginLeft: 15 }}>
+              Retreat Cost: {card.convertedRetreatCost}{' '}
+            </p>
           </div>
         </article>
         <aside className="aside aside-1">
